6d33c072137b1901d21af58e205abf921533ae21
Haythem Hammour <hammour@gmail.com>
2016-03-07 16:03:11 -0600

    Adventure mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 var currentAge = parseFloat(prompt("What is your current age?"));
 var retirementAge = parseFloat(prompt("At what age would you like to retire?"));
 var left = retirementAge-currentAge;
-document.write("<div>You have "+ left +" years left until you can retire<div>");
-var today = new Date();
-var thisYear = today.getFullYear();
-var retirementYear = thisYear + left;
-document.write("<div>It's " + thisYear + ", so you can retire in " + retirementYear +"</div>") ;
+if  (left >= 0) {
+	document.write("<div>You have "+ left +" years left until you can retire<div>");
+	var today = new Date();
+	var thisYear = today.getFullYear();
+	var retirementYear = thisYear + left;
+	document.write("<div>It's " + thisYear + ", so you can retire in " + retirementYear +"</div>") ;
+}
+else {
+	document.write("You should retire now!");
+}
 
 
